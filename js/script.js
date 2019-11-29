40948abae4cb0cb51d9cbf07c4e3dbd4cdf9343f
DhanawatS <shubhrit.dhanawat@cognizant.com>
2019-11-29 14:42:58 +0530

    removed useless buttons

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-const controls = document.querySelector('.controls');
+// const controls = document.querySelector('.controls');
 const cameraOptions = document.querySelector('.video-options>select');
 const video = document.querySelector('video');
 const canvas = document.querySelector('canvas');
 const screenshotImage = document.querySelector('img');
-const buttons = [...controls.querySelectorAll('button')];
+// const buttons = [...controls.querySelectorAll('button')];
 let streamStarted = false;
 
-const [play, pause, screenshot] = buttons;
+// const [play, pause, screenshot] = buttons;
 
 const constraints = {
     video: {
@@ -34,40 +34,7 @@ cameraOptions.onchange = () => {
     startStream(updatedConstraints);
 };
 
-play.onclick = () => {
-    if (streamStarted) {
-        video.play();
-        play.classList.add('d-none');
-        pause.classList.remove('d-none');
-        return;
-    }
-    if ('mediaDevices' in navigator && navigator.mediaDevices.getUserMedia) {
-        const updatedConstraints = {
-            ...constraints,
-            deviceId: {
-                exact: cameraOptions.value
-            }
-        };
-        startStream(updatedConstraints);
-    }
-};
 
-const pauseStream = () => {
-    video.pause();
-    play.classList.remove('d-none');
-    pause.classList.add('d-none');
-};
-
-const doScreenshot = () => {
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    canvas.getContext('2d').drawImage(video, 0, 0);
-    screenshotImage.src = canvas.toDataURL('image/webp');
-    screenshotImage.classList.remove('d-none');
-};
-
-pause.onclick = pauseStream;
-screenshot.onclick = doScreenshot;
 
 const startStream = async (constraints) => {
     const stream = await navigator.mediaDevices.getUserMedia(constraints);
@@ -77,10 +44,6 @@ const startStream = async (constraints) => {
 
 const handleStream = (stream) => {
     video.srcObject = stream;
-    play.classList.add('d-none');
-    pause.classList.remove('d-none');
-    screenshot.classList.remove('d-none');
-
 };
 
 
