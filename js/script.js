8bb749600e1a65db3da4a5e593402c035d935235
DhanawatS <shubhrit.dhanawat@cognizant.com>
2019-11-29 15:49:16 +0530

    Made changes

--- js/script.js
+++ js/script.js
@@ -1,59 +1,57 @@
-// const controls = document.querySelector('.controls');
-const cameraOptions = document.querySelector('.video-options>select');
-const video = document.querySelector('video');
-const canvas = document.querySelector('canvas');
-const screenshotImage = document.querySelector('img');
-// const buttons = [...controls.querySelectorAll('button')];
-let streamStarted = false;
+const videoElement = document.querySelector('video');
+const audioSelect = document.querySelector('select#audioSource');
+const videoSelect = document.querySelector('select#videoSource');
 
-// const [play, pause, screenshot] = buttons;
+navigator.mediaDevices.enumerateDevices()
+    .then(gotDevices).then(getStream).catch(handleError);
 
-const constraints = {
-    video: {
-        width: {
-            min: 1280,
-            ideal: 1920,
-            max: 2560,
-        },
-        height: {
-            min: 720,
-            ideal: 1080,
-            max: 1440
-        },
+audioSelect.onchange = getStream;
+videoSelect.onchange = getStream;
+
+function gotDevices(deviceInfos) {
+    for (let i = 0; i !== deviceInfos.length; ++i) {
+        const deviceInfo = deviceInfos[i];
+        const option = document.createElement('option');
+        option.value = deviceInfo.deviceId;
+        if (deviceInfo.kind === 'audioinput') {
+            option.text = deviceInfo.label ||
+                'microphone ' + (audioSelect.length + 1);
+            audioSelect.appendChild(option);
+        } else if (deviceInfo.kind === 'videoinput') {
+            option.text = deviceInfo.label || 'camera ' +
+                (videoSelect.length + 1);
+            videoSelect.appendChild(option);
+        } else {
+            console.log('Found another kind of device: ', deviceInfo);
+        }
     }
-};
+}
 
-cameraOptions.onchange = () => {
-    const updatedConstraints = {
-        ...constraints,
-        deviceId: {
-            exact: cameraOptions.value
+function getStream() {
+    if (window.stream) {
+        window.stream.getTracks().forEach(function (track) {
+            track.stop();
+        });
+    }
+
+    const constraints = {
+        audio: {
+            deviceId: { exact: audioSelect.value }
+        },
+        video: {
+            deviceId: { exact: videoSelect.value }
         }
     };
 
-    startStream(updatedConstraints);
-};
+    navigator.mediaDevices.getUserMedia(constraints).
+        then(gotStream).catch(handleError);
+}
 
+function gotStream(stream) {
+    window.stream = stream; // make stream available to console
+    videoElement.srcObject = stream;
+}
 
-
-const startStream = async (constraints) => {
-    const stream = await navigator.mediaDevices.getUserMedia(constraints);
-    handleStream(stream);
-};
-
-
-const handleStream = (stream) => {
-    video.srcObject = stream;
-};
-
-
-const getCameraSelection = async () => {
-    const devices = await navigator.mediaDevices.enumerateDevices();
-    const videoDevices = devices.filter(device => device.kind === 'videoinput');
-    const options = videoDevices.map(videoDevice => {
-        return `<option value="${videoDevice.deviceId}">${videoDevice.label}</option>`;
-    });
-    cameraOptions.innerHTML = options.join('');
-};
-
-getCameraSelection();
+function handleError(error) {
+    console.error('Error: ', error);
+}
